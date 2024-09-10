63f8af1d6dbb4082b47d9d6d8b685f2417daebb3
Otávio Gabriel <107057360+Otavioalv@users.noreply.github.com>
2024-09-09 21:03:17 -0400

    chore: update bizzare bug tomorrow

--- frontend/pages/task.js
+++ frontend/pages/task.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', async() => {
 });
 
 async function getValuesFormTask(e) {
-    e.preventDefault();
+    e.preventDefault(); 
 
     const form = document.getElementById('taskForm');
     const formData = new FormData(form);
@@ -15,9 +15,32 @@ async function getValuesFormTask(e) {
     const description = formData.get('descricao');
     const name = formData.get('nome');
 
+    if(!title || !description || !name || !status) {
+        alert("Preencha os campos corretamente!!!");
+        return;
+    }
+
     await createTaskController(title, description, name);
 }
 
+async function getValuesFormUpdate(e, id) {
+    e.preventDefault();
+
+    const form = document.getElementById(`taskFormUpdate-${id}`);
+    const formData = new FormData(form);
+
+    const title = formData.get('titulo');
+    const description = formData.get('descricao');
+    const name = formData.get('nome');
+    const status = formData.get(`status-${id}`);
+
+    if(!title || !description || !name || !status) {
+        alert("Preencha os campos corretamente!!!");
+        return;
+    }
+
+    await updateTaskController(title, description, name, status, id);
+}
 
 
 // Controllers functions
@@ -42,8 +65,6 @@ async function listTaskController(id="") {
     if(id)
         url = url + "?id=" + id;
 
-    console.log(url);
-
     const result = await fetch(url, {
         method: "GET"
     })
@@ -52,6 +73,22 @@ async function listTaskController(id="") {
     });
 
     return result.list;
+}
+
+async function updateTaskController(title, description, name, status, id) {
+    
+    const paramsUrl = `?id=${id}&title=${title.trim()}&description=${description.trim()}&member_name=${name.trim()}&status=${status}`;
+    const url = URL + paramsUrl;
+
+    const result = await fetch(url, {
+        method: 'PUT',
+    })
+    .then(async res => {
+        return await res.json();
+    })
+
+    const list = await listTaskController();
+    addTaskInTaskList(list);
 }
 
 
@@ -118,7 +155,6 @@ async function updateButton(id) {
     const name = list[0].member_name;
     const status = list[0].status;
 
-    console.log(divTaskItem);
 
     const formTaskUpdate = `
          <div class="task-bar">
@@ -126,17 +162,17 @@ async function updateButton(id) {
             <img src="../assets/images/bin.svg" alt="lixeira" draggable="false">
         </button>
         
-        <button class="bg-blue-600 hover:bg-blue-700 transition p-1 rounded">
+        <button onclick="returnTaskCard(${id})" class="bg-blue-600 hover:bg-blue-700 transition p-1 rounded">
             <div class="cancel-edit"></div>
         </button>
     </div>
 
-    <form class="form-update-task">
+    <form class="form-update-task" id="taskFormUpdate-${id}">
         <div>
-            <label for="titulo">TÍTULO</label>
+            <label for="titulo-${id}">TÍTULO</label>
             <input 
                 type="text" 
-                id="titulo" 
+                id="titulo-${id}" 
                 name="titulo"
                 placeholder="Título"
                 class="outline-none p-3 rounded-sm text-rose-600 focus:outline-rose-600 w-full"
@@ -145,10 +181,10 @@ async function updateButton(id) {
         </div>
 
         <div>
-            <label for="nome">NOME DO MEMBRO</label>
+            <label for="nome-${id}">NOME DO MEMBRO</label>
             <input 
                 type="text" 
-                id="nome" 
+                id="nome-${id}" 
                 name="nome"
                 placeholder="Nome do membro"
                 class="outline-none p-3 rounded-sm text-rose-600 focus:outline-rose-600 w-full"
@@ -161,212 +197,102 @@ async function updateButton(id) {
             <div class="">
                 
                 <input 
-                    id="pendent" 
+                    id="pendent-${id}" 
                     type="radio"
                     value="Pendent" 
-                    name="status"
-                    ${status === "Pendent" ? 'checked' : ""}
+                    name="status-${id}"
+                    ${status === "Pendent" ? 'checked' : ''}
                 >
                 
                 
                 <label 
-                    for="pendent" 
+                    for="pendent-${id}" 
                     class=""
                 >Pendent</label>
             </div>
             
             <div class="">
                 <input 
-                    checked id="in-progress" 
+                    id="in-progress-${id}" 
                     type="radio" 
                     value="In progress" 
-                    name="status" 
-                    class=""
+                    name="status-${id}" 
                     ${status === "In progress" ? 'checked' : ""}
                 >
                 <label 
-                    for="in-progress" 
+                    for="in-progress-${id}" 
                     class=""
                 >In progress</label>
             </div>
 
             <div class="">
                 <input 
-                    checked id="completed" 
+                    id="completed-${id}" 
                     type="radio" 
                     value="Completed" 
-                    name="status" 
-                    class=""
+                    name="status-${id}" 
                     ${status === "Completed" ? 'checked' : ""}
                 >
                 <label 
-                    for="completed" 
+                    for="completed-${id}" 
                     class=""
                 >Completed</label>
             </div>
+
+             <div>
+                <label for="descricao-${id}">DESCRIÇÃO</label>
+                <textarea 
+                    type="text" 
+                    id="descricao-${id}" 
+                    name="descricao"
+                    placeholder="Descrição"
+                    class="outline-none p-3 rounded-sm text-rose-600 focus:outline-rose-600 w-full"
+                >${description}</textarea>
+            </div>
+
+            <button onclick="getValuesFormUpdate(event, ${id})" class="bg-blue-600 hover:bg-blue-800 focus:outline-blue-600 py-2 px-3 rounded-full transition my-5 w-full">ATUALIZAR</button>
         </div>
     `
     
     divTaskItem.innerHTML = formTaskUpdate;
-
 }
 
+async function returnTaskCard(id) {
+    const taskItem = document.getElementById(`task-item-${id}`);
+    const list = await listTaskController(id);
+    
+    const title = list[0].title;
+    const description = list[0].description;
+    const name = list[0].member_name;
+    const status = list[0].status;
 
 
 
-/* 
-
-<div class="task-item bg-red-500">
-                            
-    <div class="task-bar">
-            <button class="bg-red-600 hover:bg-red-700 transition p-1 rounded">
-                <img src="../assets/images/bin.svg" alt="lixeira" draggable="false">
-            </button>
-            
-            <button class="bg-blue-600 hover:bg-blue-700 transition p-1 rounded">
-                <img src="../assets/images/edit-icon.svg" alt="editar" draggable="false">
-            </button>
-    </div>
-
-    <h1 class="bg-red-600 sm:p-2 p-1 rounded">Titledddddddddddddddddddddddddddd</h1>
-    <h2 class="bg-red-600 sm:p-2 p-1 rounded">member name</h2>
-    <p class="bg-red-600 sm:p-2 p-1 rounded">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea corporis deleniti ipsa est minima necessitatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus fugit maiores nostrum cupiditate. Consequatur voluptatum voluptate recusandae, temporibus facere voluptates vero. Culpa soluta, quia ea eaque aliquam non facilis quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quam reiciendis repellat mollitia numquam est accusamus, ex consequatur placeat impedit amet pariatur officia consequuntur voluptatem ea nemo quisquam, debitis ad?</p>
-</div>
-
-
-
-<div class="task-item bg-red-500">
-    <div class="task-bar">
-        <button class="bg-red-600 hover:bg-red-700 transition p-1 rounded">
-            <img src="../assets/images/bin.svg" alt="lixeira" draggable="false">
-        </button>
-        
-        <button class="bg-blue-600 hover:bg-blue-700 transition p-1 rounded">
-            <div class="cancel-edit"></div>
-        </button>
-    </div>
-
-    <form class="form-update-task">
-        <div>
-            <label for="titulo">TÍTULO</label>
-            <input 
-                type="text" 
-                id="titulo" 
-                name="titulo"
-                placeholder="Título"
-                class="outline-none p-3 rounded-sm text-rose-600 focus:outline-rose-600 w-full"
-            >
-        </div>
-
-        <div>
-            <label for="nome">NOME DO MEMBRO</label>
-            <input 
-                type="text" 
-                id="nome" 
-                name="nome"
-                placeholder="Nome do membro"
-                class="outline-none p-3 rounded-sm text-rose-600 focus:outline-rose-600 w-full"
-            >
-        </div>
-
-        <div class="status-options">
-            <label>STATUS</label>
-            <div class="">
-                
-                <input 
-                    id="pendent" 
-                    type="radio"
-                    value="Pendent" 
-                    name="status" 
-                >
-                
-                
-                <label 
-                    for="pendent" 
-                    class=""
-                >Pendent</label>
-            </div>
-            
-            <div class="">
-                <input 
-                    checked id="in-progress" 
-                    type="radio" 
-                    value="In progress" 
-                    name="status" 
-                    class=""
-                >
-                <label 
-                    for="in-progress" 
-                    class=""
-                >In progress</label>
-            </div>
-
-            <div class="">
-                <input 
-                    checked id="completed" 
-                    type="radio" 
-                    value="Completed" 
-                    name="status" 
-                    class=""
-                >
-                <label 
-                    for="completed" 
-                    class=""
-                >Completed</label>
-            </div>
-
-        </div>
-
-        
-
-        <div>
-            <label for="descricao">DESCRIÇÃO</label>
-            <textarea 
-                type="text" 
-                id="descricao" 
-                name="descricao"
-                placeholder="Descrição"
-                class="outline-none p-3 rounded-sm text-rose-600 focus:outline-rose-600 w-full"
-            ></textarea>
-        </div>
-        <button class="bg-blue-600 hover:bg-blue-800 focus:outline-blue-600 py-2 px-3 rounded-full transition my-5 w-full">ATUALIZAR</button>
-    </form>
-</div>
-
-
-
-
-<div class="itens-tasks">
-    <div class="task-item bg-yellow-300">    
+    const taskBar = `
         <div class="task-bar">
             <button class="bg-red-600 hover:bg-red-700 transition p-1 rounded">
                 <img src="../assets/images/bin.svg" alt="lixeira" draggable="false">
             </button>
             
-            <button class="bg-blue-600 hover:bg-blue-700 transition p-1 rounded">
+            <button onclick="updateButton(${id})" class="bg-blue-600 hover:bg-blue-700 transition p-1 rounded">
                 <img src="../assets/images/edit-icon.svg" alt="editar" draggable="false">
             </button>
         </div>
-        <h1 class="bg-yellow-400 sm:p-2 p-1 rounded">Title</h1>
-        <h2 class="bg-yellow-400 sm:p-2 p-1 rounded">member name</h2>
-        <p class="bg-yellow-400 sm:p-2 p-1 rounded">Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt magni sint veritatis natus, consequatur, autem soluta nostrum qui est aspernatur voluptate deleniti, dolores labore omnis at! Sit nostrum eaque deserunt! ipsum dolor sit amet consectetur, adipisicing elit. Ea corporis deleniti ipsa est minima necessitatibus.</p>
-    </div>
-</div>
+    `;
 
- <div class="itens-tasks">
-    <div class="task-item bg-green-400">    
-        <div class="task-bar">
-            <button class="bg-red-600 hover:bg-red-700 transition p-1 rounded">
-                <img src="../assets/images/bin.svg" alt="lixeira" draggable="false">
-            </button>
-            
-            <button class="bg-blue-600 hover:bg-blue-700 transition p-1 rounded">
-                <img src="../assets/images/edit-icon.svg" alt="editar" draggable="false">
-            </button>
-        </div>
-        <h1 class="bg-green-600 sm:p-2 p-1 rounded">Title</h1>
-        <h2 class="bg-green-600 sm:p-2 p-1 rounded">member name</h2>
-        <p class="bg-green-600 sm:p-2 p-1 rounded">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea corporis deleniti ipsa est minima necessitatibus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, numquam maiores. Numquam voluptate sequi unde nostrum dicta ratione id, nam error temporibus saepe. Quaerat natus consequuntur accusantium mollitia vel modi. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum, nihil ullam fugiat velit eaque fugit accusantium. Quibusdam aliquid obcaecati, delectus quia eaque voluptates corporis aliquam quidem tempore, sit voluptatum reprehenderit?</p>
-    </div>
-</div>
-*/
+    if(status === "Pendent"){
+        classTextTask = "bg-red-600 sm:p-2 p-1 rounded";
+    } else if(status === "In progress") {
+        classTextTask = "bg-yellow-400 sm:p-2 p-1 rounded";
+    } else if(status === "Completed"){
+        classTextTask = "bg-green-600 sm:p-2 p-1 rounded";
+    }  
+
+    const infoTask = `
+        <h1 class="${classTextTask}">${title}</h1>
+        <h2 class="${classTextTask}">${name}</h2>
+        <p class="${classTextTask}">${description}</p>
+    `;
+
+    taskItem.innerHTML = taskBar + infoTask;
+}
